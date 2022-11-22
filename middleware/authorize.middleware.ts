c39c8f5d9ed8d3f49aedc38a9dee3689d6ac94ed
stefan.jambrich <nutela415@gmail.com>
2022-11-22 13:35:55 +0100

    Started work on verify route for token

---
+++ middleware/authorize.middleware.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+const jwt = require('jsonwebtoken');
+
+export const authorize = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    if (!token) return res.status(403).send('No token was provided');
+
+    jwt.verify(token, process.env.SECRET, (err: string, decoded: string) => {
+        if (err) return res.status(401).send('Unathuorized access')
+
+        
+    })
+}
