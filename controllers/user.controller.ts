acbad2f42ba8c9c449ba178e3f1d0786d907b5ed
stefan.jambrich <nutela415@gmail.com>
2022-11-30 14:19:59 +0100

    Added edit route

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 
 const User = require('../model/user.model');
 
-interface User {
+interface IUser {
     firstName: string,
     lastName: string,
     nickaname: string,
@@ -12,7 +12,7 @@ interface User {
 }
 
 export const getUser = async (req: Request, res: Response) => {
-    const { userId } = req.params
+    const { userId } = req.params;
  
     const user = await User.findOne({
         where: {
@@ -23,4 +23,33 @@ export const getUser = async (req: Request, res: Response) => {
     if (!user) return res.status(404).send('Invalid user');
 
     return res.status(200).send(JSON.stringify(user));
+}
+
+export const editUser = async(req: Request, res: Response) => {
+    const data = req.body as IUser;
+    const {userId} = req.params;
+
+    if (!data) return res.status(400).send('No data were sent');
+    
+    const user = await User.findOne({
+        where: {
+            postId: userId
+        }
+    });
+
+    if (!user) return res.status(404).send('Invalid user id');
+
+    await User.update({
+        firstName: data.firstName,
+        lastName: data.lastName,
+        nickname: data.nickaname,
+        description: data.description
+    }, {
+        where: {
+            postId: userId
+        }
+    })
+
+    await User.sync();
+    return res.status(400).send('User updated succesfully');
 }
