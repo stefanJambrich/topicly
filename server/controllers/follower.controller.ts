6c8326e00f0cd4842f8ac93558f22ab346c709fd
stefan.jambrich <nutela415@gmail.com>
2022-12-26 13:36:11 +0100

    Follower creation working

--- server/controllers/follower.controller.ts
+++ server/controllers/follower.controller.ts
@@ -2,28 +2,25 @@ import { Request, Response } from "express";
 
 const User = require('../model/user.model');
 const Follower = require('../model/follower.model');
+const UserFollower = require('../model/userFollower.model');
 
 export const follow = async (req: Request, res: Response) => {
     const data = req.body;
 
     console.log(data.userId)
+    console.log(data.followerId)
 
     if (!data.userId || !data.followerId) return res.status(400).send('Missing data');
 
-    const user = await User.findOne({ where: { userId: data.userId } });
-    if(!user) return res.status(404).send('Invalid user');
-
-    const follower = await User.findOne({ where: { userId: data.followerId } });
-    if(!follower) return res.status(404).send('Invalid follower');
-
-    console.log(user);
-    console.log(follower);
-
-    const follow = await Follower.create({
-        followerId: user.id,
-        userId: follower.id
+    const user = await User.findOne({where: { userId: data.userId }});
+    const userF = await User.findOne({ where: { userId: data.followerId }});
+    const follower = await Follower.create({
+        usersTableId: userF.dataValues.id
     });
-
-    await Follower.sync();
-    return res.status(200).send('Followed successfully');
+    const userFollower = await UserFollower.create({
+        usersTableId: user.dataValues.id,
+        followerEntityId: follower.dataValues.id
+    })
+    
+    return res.status(200).send("Juch")
 }
