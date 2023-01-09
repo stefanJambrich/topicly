2ef17893fe48b0dc9cb6933d081b6cab5db74c17
stefan.jambrich <nutela415@gmail.com>
2023-01-09 18:12:24 +0100

    Added verify route for JWT

--- server/routes/auth.route.ts
+++ server/routes/auth.route.ts
@@ -1,10 +1,11 @@
 import express  from "express";
-import { login, logout, register } from "../controllers/auth.controller";
+import { authorization, login, logout, register } from "../controllers/auth.controller";
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/register", register);
 router.post('/logout', logout);
+router.post('/verify', authorization);
 
 module.exports = router;
